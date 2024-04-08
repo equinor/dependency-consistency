a2c4cf1b46cea9c8f2bfc93e4fcc41aebbd263d7
Sindre Nistad <snis@equinor.com>
2024-04-08 13:46:48 +0200

    refactor: Use the xo style

--- scripts/keep-own-dependencies-up-to-date.cjs
+++ scripts/keep-own-dependencies-up-to-date.cjs
@@ -2,9 +2,9 @@
 // @ts-check
 
 /** @constant */
-const YAML = require("yaml")
-const fs = require("node:fs")
-const { parseVersion, readFile } = require("../shared.cjs")
+const YAML = require('yaml');
+const fs = require('node:fs');
+const {parseVersion, readFile} = require('../shared.cjs');
 
 /**
  * @typedef Dependency
@@ -16,33 +16,34 @@ const { parseVersion, readFile } = require("../shared.cjs")
  * */
 
 /** @type {Record<string, Dependency> } */
-const dependencies = require("../package-lock.json").dependencies
+const dependencies = require('../package-lock.json').dependencies;
 
 /**
  * @type {{additional_dependencies: string[]}[]}
  */
-const hooks = YAML.parse(readFile('.pre-commit-hooks.yaml'))
+const hooks = YAML.parse(readFile('.pre-commit-hooks.yaml'));
 
 hooks.forEach(hook => {
-    /** @type {string[]} */
-    const updatedDependencies = []
-    hook.additional_dependencies.forEach(dependency => {
-        const { name, version } = parseVersion(dependency)
+	/** @type {string[]} */
+	const updatedDependencies = [];
+	hook.additional_dependencies.forEach(dependency => {
+		const {name, version} = parseVersion(dependency);
 
-        /** @type {Dependency | undefined} */
-        const reference = dependencies[name]
-        if (!(reference)) {
-            throw new Error(`Missing dependency '${name}' in package-lock.json`)
-        }
-        if (reference.version !== version) {
-            updatedDependencies.push(`${name}@${reference.version}`)
-        } else {
-            updatedDependencies.push(dependency)
-        }
-    })
-    hook.additional_dependencies = updatedDependencies
-})
+		/** @type {Dependency | undefined} */
+		const reference = dependencies[name];
+		if (!(reference)) {
+			throw new Error(`Missing dependency '${name}' in package-lock.json`);
+		}
 
-fs.writeFileSync(".pre-commit-hooks.yaml", YAML.stringify(hooks, {
-    indent: 2,
+		if (reference.version !== version) {
+			updatedDependencies.push(`${name}@${reference.version}`);
+		} else {
+			updatedDependencies.push(dependency);
+		}
+	});
+	hook.additional_dependencies = updatedDependencies;
+});
+
+fs.writeFileSync('.pre-commit-hooks.yaml', YAML.stringify(hooks, {
+	indent: 2,
 }));
