09aafeeef122a7d2ad1dccd7201fad26e553305a
Sindre Nistad <snis@equinor.com>
2024-04-08 17:31:31 +0200

    feat: Support all Node.js additional_hooks

--- index.cjs
+++ index.cjs
@@ -3,6 +3,9 @@ const path = require('node:path');
 const lockfile = require('@yarnpkg/lockfile');
 const YAML = require('yaml');
 const semverSort = require('semver-sort');
+const sqlite3 = require('sqlite3');
+const {open} = require('sqlite');
+
 const {readFile, parseVersion} = require('./shared.cjs');
 
 if (process.argv.length < 3) {
@@ -11,6 +14,13 @@ if (process.argv.length < 3) {
 
 const PRE_COMMIT_YAML = '.pre-commit-config.yaml';
 const LOCK_FILE = process.argv[2];
+
+/** @typedef {'node'} SupportedLanguages */
+const SUPPORTED_LANGUAGES = /** @type {const} */ (['node']);
+
+
+/** @type {Awaited<ReturnType<open>> | null} */
+let db = null;
 
 /**
  * @param {string} file
@@ -60,7 +70,7 @@ function parseYarn3LockFile(file) {
 	return Object.keys(dependencies).reduce((mapping, dependency) => {
 		if (dependency === '__metadata') return mapping;
 
-		const parts =  dependency.split('@');
+		const parts = dependency.split('@');
 		const name = !parts[0] ? '@' + parts[1] : parts[0];
 		const installedVersion = dependencies[dependency].version;
 		addElement(mapping, name, installedVersion);
@@ -77,10 +87,10 @@ function parseYarn3LockFile(file) {
  */
 function parsePackageLockFile(file) {
 	/**
-	 * @typedef Lockfile
-	 * @property {number} lockfileVersion
-	 * @property {Record<string, { version: string }>} packages
-	 */
+     * @typedef Lockfile
+     * @property {number} lockfileVersion
+     * @property {Record<string, { version: string }>} packages
+     */
 	/** @type {Lockfile} */
 	const dependencies = JSON.parse(readFile(file));
 	if (![2, 3].includes(dependencies.lockfileVersion)) throw new Error(`Unsupported version of package-lock.json (${dependencies.lockfileVersion})`);
@@ -139,57 +149,105 @@ function getDependencies(lockFile) {
 const dependencies = getDependencies(LOCK_FILE);
 
 /**
- * @returns {void}
+ * @typedef Hook
+ * @property {string} id
+ * @property {string[]} [additional_dependencies]
+ * @property {SupportedLanguages} [language]
+ *
+ * @typedef Repo
+ * @property {string} repo
+ * @property {string} rev
+ * @property {Hook[]} hooks
+ *
+ * @typedef PreCommit
+ * @property {Repo[]} repos
  */
-function updateDependencies() {
 
-	/**
-     * @typedef Hook
-     * @property {string} id
-     * @property {string[]} additional_dependencies
-     *
-     * @typedef Repo
-     * @property {string} repo
-     * @property {Hook[]} hooks
-     *
-     * @typedef PreCommit
-     * @property {Repo[]} repos
-    */
+/**
+ *  @param {Repo} repo The repository for these hooks
+ *  @param {Hook} hook The specific hook we are evaluating
+ *  @returns {Promise<SupportedLanguages | null>}
+ *  */
+async function getHookLanguage(repo, hook) {
+	// TODO: Check if local
+	if (repo.repo === 'local') {
+		return hook.language ?? null;
+	} else if (hook.language) {
+		return hook.language;
+	}
 
+	if (db === null) {
+		db = await open({
+			filename: process.env.HOME + '/.cache/pre-commit/db.db',
+			driver: sqlite3.Database,
+		});
+	}
+
+	const longReference = hook.additional_dependencies ? repo.repo + ':' + hook.additional_dependencies.join(',') : repo.repo;
+	const paths = await db.all('select path from repos where repo = ? and ref = ?;', longReference, repo.rev);
+	if (paths.length > 0) {
+		/** @type {{path: string | undefined}} */
+		const {path} = paths[0];
+		if (!path) {
+			console.warn(`${repo.repo} (hook: ${hook.id}) is not installed in pre-commit`);
+			return null;
+		}
+
+		/** @type {{id: string, language: SupportedLanguages}[]} */
+		const preCommitConfiguration = YAML.parse(readFile(path + '/.pre-commit-hooks.yaml'));
+		const {language} = preCommitConfiguration.find(({id}) => id === hook.id) ?? {
+			language: null,
+		};
+		return language;
+	}
+
+	return null;
+}
+
+/**
+ * @returns {Promise<void>}
+ */
+async function updateDependencies() {
 	/** @type {PreCommit} */
 	const preCommit = YAML.parse(readFile(PRE_COMMIT_YAML));
 
-	const eslintHook = preCommit.repos
-		.find((obj) => obj.repo === 'https://github.com/pre-commit/mirrors-eslint')
-		?.hooks.find((hook) => hook.id === 'eslint');
-	if (eslintHook?.additional_dependencies) {
-		const mapping = eslintHook.additional_dependencies.reduce(
-			(
-				/** @type {Record<string, string>} */
-				mapping,
-				dependency,
-			) => {
-				const {name} = parseVersion(dependency);
-				const installedVersions = dependencies[name];
-				if (!name) {
-					console.warn(`${dependency} is used in in a pre-commit hook, but is not in ${LOCK_FILE}`);
-				}
+	preCommit.repos.forEach(repo => {
+		repo.hooks.forEach(async hook => {
+			const hookLanguage = await getHookLanguage(repo, hook);
+			if (hookLanguage === null || !SUPPORTED_LANGUAGES.includes(hookLanguage)) return;
 
-				mapping[dependency] = installedVersions
-					? `${name}@${installedVersions[0]}`
-					: dependency;
-				return mapping;
-			},
-			/** @type {Record<string, string>} */
-			{},
-		);
-		let content = readFile(PRE_COMMIT_YAML);
-		Object.keys(mapping).forEach((previousVersion) => {
-			const newVersion = mapping[previousVersion];
-			content = content.replace(new RegExp(`( +- *["']?)${previousVersion}(["']? *#.*)?`, 'gi'), '$1' + newVersion + '$2');
+			if (hook?.additional_dependencies) {
+				const mapping = hook.additional_dependencies.reduce(
+					(
+						/** @type {Record<string, string>} */
+						mapping,
+						dependency,
+					) => {
+						const {name} = parseVersion(dependency);
+						const installedVersions = dependencies[name];
+						if (!name) {
+							console.warn(`${dependency} is used in in a pre-commit hook, but is not in ${LOCK_FILE}`);
+						}
+
+						mapping[dependency] = installedVersions
+							? `${name}@${installedVersions[0]}`
+							: dependency;
+						return mapping;
+					},
+					/** @type {Record<string, string>} */
+					{},
+				);
+				let content = readFile(PRE_COMMIT_YAML);
+				Object.keys(mapping).forEach((previousVersion) => {
+					const newVersion = mapping[previousVersion];
+					content = content.replace(new RegExp(`( +- *["']?)${previousVersion}(["']? *#.*)?`, 'gi'), '$1' + newVersion + '$2');
+				});
+				fs.writeFileSync(PRE_COMMIT_YAML, content);
+			}
 		});
-		fs.writeFileSync(PRE_COMMIT_YAML, content);
-	}
+	});
 }
 
-updateDependencies();
+(async () => {
+	await updateDependencies();
+})();
