5ee8894b5d06c3303b253b26e7e52cd40f0d6db3
Sindre Nistad <snis@equinor.com>
2024-04-08 12:53:43 +0200

    fix: Improve type annotation

--- scripts/keep-own-dependencies-up-to-date.cjs
+++ scripts/keep-own-dependencies-up-to-date.cjs
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
+// @ts-check
+
+/** @constant */
 const YAML = require("yaml")
 const fs = require("node:fs")
 const { parseVersion, readFile } = require("../shared.cjs")
 
-const { dependencies } = require("../package-lock.json")
+/**
+ * @typedef Dependency
+ * @property {string} version
+ * @property {string} resolved
+ * @property {string} integrity
+ * @property {boolean} [dev]
+ * @property {any} [requires]
+ * */
+
+/** @type {Record<string, Dependency> } */
+const dependencies = require("../package-lock.json").dependencies
 
 /**
  * @type {{additional_dependencies: string[]}[]}
@@ -15,6 +28,8 @@ hooks.forEach(hook => {
     const updatedDependencies = []
     hook.additional_dependencies.forEach(dependency => {
         const { name, version } = parseVersion(dependency)
+
+        /** @type {Dependency | undefined} */
         const reference = dependencies[name]
         if (!(reference)) {
             throw new Error(`Missing dependency '${name}' in package-lock.json`)
